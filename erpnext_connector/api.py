e9bf7546d77cc6462fc76073ebc6812145296e61
Revant Nandgaonkar <revant.one@gmail.com>
2018-02-07 17:49:09 +0530

    added implict grant stub

--- erpnext_connector/api.py
+++ erpnext_connector/api.py
@@ -56,3 +56,99 @@ def get_upstream_docs(doctype, fields, page_len, filters):
 			doc.save(ignore_permissions=True)
 			frappe.db.commit()
 	return data
+
+def get_token_page():
+	javascript = """$(() => {
+		const route = localStorage.getItem("microservice_route");
+		if (route) {
+			const go_to = window.location.origin
+			+ route + "?access_token="
+			+ get_parameter_by_name("access_token");
+			window.location.href = go_to;
+		}
+	});
+
+	get_parameter_by_name(name) => {
+		name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+		let regex = new RegExp("[\\#&]" + name + "=([^&#]*)"),
+		results = regex.exec(location.hash);
+		return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+	}
+	"""
+	token_page =  {
+		"doctype": "Web Page",
+		"insert_code" : 1,
+		"insert_style" : 0,
+		"javascript": javascript,
+		"main_section" : "<div style=\"text-align: center;\">Authorizing ...</div>",
+		"name" : "token",
+		"published" : 1,
+		"route" : "token",
+		"show_sidebar" : 0,
+		"show_title": 0,
+		"title": "token"
+	}
+
+def get_custom_script():
+	custom_script = """
+		authorize(server_url, client_id, redirect_uri) => {
+			console.log("Authorizing...");
+
+			// queryStringData is details of OAuth Client (Implicit Grant) on Custom App
+			let queryStringData = {
+				response_type : "token",
+				client_id : client_id,
+				redirect_uri : redirect_uri
+			}
+
+			// Clear previously set microservice_route if any
+			localStorage.removeItem("microservice_route");
+
+			// Get current raw route and build url
+			const route = "/desk#" + frappe.get_raw_route_str();
+
+			// Set route in localStorage
+			localStorage.setItem("microservice_route", route);
+
+			// Go authorize!
+			window.location.replace(server_url + "/api/method/frappe.integrations.oauth2.authorize?" + jQuery.param(queryStringData));
+		}
+
+		consume_token(endpoint, data, method) => {
+			const token = frappe.urllib.get_arg("access_token");
+			const route = localStorage.getItem("microservice_route");
+
+			// if page has route with parameter ?access_token=420 and microservice_route is in localStorage.
+			if (token && route) {
+
+				// Clean up access token from route
+				frappe.set_route(frappe.get_route().join("/"))
+
+				// query protected resource e.g. openid_profile with token
+				let call = {
+					"async": true,
+					"crossDomain": true,
+					"url": endpoint,
+					"method": method || "GET",
+					"data": {
+						"data": JSON.stringify(data)
+					},
+					"headers": {
+						"authorization": "Bearer " + token,
+						"content-type": "application/x-www-form-urlencoded"
+					}
+				}
+
+				$.ajax(call).done(function (response) {
+					// display response
+					console.log(response);
+
+					frappe.show_alert(response)
+
+					// clear microservice_route from localStorage
+					localStorage.removeItem("microservice_route");
+				});
+			}
+		}
+	"""
+	return custom_script
