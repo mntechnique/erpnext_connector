a67249a53e70583410ce54f98525a7e244449498
Gaurav Naik <gvrvnk@gmail.com>
2018-03-19 19:22:58 +0530

    Added doctype ERPNext Branch

---
+++ erpnext_connector/erpnext_connector/doctype/erpnext_branch/erpnext_branch.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, MN Technique and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('ERPNext Branch', {
+	refresh: function(frm) {
+
+	}
+});
